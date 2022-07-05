2706af1eb7f0805f12cf2d34e561d6f8c94da917
shiv071 <105675048+shiv071@users.noreply.github.com>
2022-07-05 10:13:23 +0530

    Create decimaltobinary.cpp

---
+++ decimaltobinary.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void dectobinary(int n){
+    int binaryNum[32];
+
+    int i=0;
+    while(n>0){
+        binaryNum[i]=n%2;
+        n=n/2;
+        i++;
+    }
+    for(int j=i-1;j>=0;j--){
+        cout<<binaryNum[j];
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the decimal number : ";
+    cin>>n;
+    dectobinary(n);
+  return 0;
+}
